a51694cb9cd08ee1f14ea8f01eb48c5e929ec227
Pascal Molli <pascal.molli@ls2n.fr>
2023-10-26 10:34:41 +0200

    data

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 
 
 ## create the cluster
-gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project master-2-large-scale-data
+gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 50 --num-workers 0 --worker-machine-type n1-standard-4 --worker-boot-disk-size 50 --image-version 2.0-debian10 --project master-2-large-scale-data
 
 ## copy data
 gsutil cp small_page_links.nt gs://myown_bucket/
@@ -17,6 +17,7 @@ gsutil cp dataproc.py gs://myown_bucket/
 
 ## Clean out directory
 gsutil rm -rf gs://myown_bucket/out
+gsutil rm -rf gs://small_page_links/out
 
 
 ## run
